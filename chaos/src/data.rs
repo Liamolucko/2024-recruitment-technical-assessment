3595738c5c7df734db2fdbf8eb4cb70e17b93918
Liam Murphy <liampm32@gmail.com>
2024-02-24 14:57:34 +1100

    complete chaos q1

--- chaos/src/data.rs
+++ chaos/src/data.rs
@@ -2,21 +2,37 @@ use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
 
 pub async fn process_data(Json(request): Json<DataRequest>) -> impl IntoResponse {
-    // Calculate sums and return response
-
-    let response = DataResponse {
-        
+    let mut response = DataResponse {
+        string_len: 0,
+        int_sum: 0,
     };
+
+    for element in request.data {
+        match element {
+            StringOrInt::String(s) => response.string_len += s.len(),
+            StringOrInt::Int(n) => response.int_sum += n,
+        }
+    }
 
     (StatusCode::OK, Json(response))
 }
 
 #[derive(Deserialize)]
 pub struct DataRequest {
-    // Add any fields here
+    data: Vec<StringOrInt>,
+}
+
+#[derive(Deserialize)]
+#[serde(untagged)]
+pub enum StringOrInt {
+    String(String),
+    Int(i64),
 }
 
 #[derive(Serialize)]
 pub struct DataResponse {
-    // Add any fields here
+    /// The sum of the lengths of all the input strings.
+    string_len: usize,
+    /// The sum of all the input integers.
+    int_sum: i64,
 }
